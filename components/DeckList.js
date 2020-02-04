1abb4df4a38e91ca30a7ca7a57bcfc227b689c22
zarakM <mughalzarak@gmail.com>
2020-02-04 12:07:47 -0800

    components architect dine

---
+++ components/DeckList.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const DeckList = () => {
+    return (  );
+}
+ 
+export default DeckList;
---
+++ components/Quiz.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Quiz = () => {
+    return (  );
+}
+ 
+export default Quiz;

15e15c7eed43e60f5b7b3ee4b3147c071665df64
zarakM <mughalzarak@gmail.com>
2020-02-04 11:33:59 -0800

    initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js kasdmaskl sada dajsdjkas d asjdssadjoadoasndoijasdioasjiodj!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
